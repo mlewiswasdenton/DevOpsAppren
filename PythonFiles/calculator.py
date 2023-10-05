3593e15b371c2549b0bdc1dc71df3fae194f059f
Michelle Denton <michelle.denton@asda.co.uk>
2023-10-05 14:35:54 +0100

    Initial upload

---
+++ PythonFiles/calculator.py
@@ -0,0 +1,14 @@
+maths_operation = input("Enter maths operation (+, -, X or /): ")
+first_number_string: str = input("Enter first number: ")
+first_number = int(first_number_string)
+second_number_string: str = input("Enter second number: ")
+second_number = int(second_number_string)
+
+if maths_operation == '+':
+    print (first_number + second_number)
+elif maths_operation == '-':
+    print (first_number - second_number)
+elif maths_operation == 'x':
+    print (first_number * second_number)
+elif maths_operation == '/':
+    print (first_number / second_number)
